17d560bf4692cf616a1305fd1791623d6912feb8
ShahHetviIT <hetvi4551@gmail.com>
2024-06-30 23:19:37 +0530

    cm

--- client1/src/utils/ApiRoutes.js
+++ client1/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:3001";
-
+// export const host = "http://localhost:3001";
+export const host = "https://todolist-gomr.onrender.com";
 export const addTodoRoute = `${host}/api/auth/addTodo`;
 export const getTodoRoute = `${host}/api/auth/getTodo`;
